d758505a4f43c2d7a8d2863eb8d3f3e539bd31b2
Piyush Choudhary <pchoudh2@ncsu.edu>
2017-11-23 00:30:10 -0500

    Model now works with decaying learning rate and regularization. Implemented methods for different types of run, like training on the entire dataset or on different train and test sets or predicting.

--- what's cooking/create_df.py
+++ what's cooking/create_df.py
@@ -7,7 +7,15 @@ def drop_na(df):
     return df.dropna()
 
 
-def make_df(file_path):
+def one_hot_to_real(one_index):
+    sorted_ingred = [u'brazilian', u'british', u'cajun_creole', u'chinese', u'filipino', u'french', u'greek', u'indian',
+                     u'irish', u'italian', u'jamaican', u'japanese', u'korean', u'mexican', u'moroccan', u'russian',
+                     u'southern_us', u'spanish', u'thai', u'vietnamese']
+
+    return sorted_ingred[one_index]
+
+
+def make_train_test_df(file_path):
     with open(file_path) as data_file:
         data = json.load(data_file)
 
@@ -16,12 +24,13 @@ def make_df(file_path):
         ingred_list |= set(item['ingredients'])
 
     ingred_list = list(ingred_list)
+    print len(ingred_list)
 
     ingred_dict = dict()
     for i, ingredient in enumerate(ingred_list):
         ingred_dict[ingredient] = i
 
-    feature_df_np = np.ones((len(data), len(ingred_list)), dtype=np.int8) * -1
+    feature_df_np = np.zeros((len(data), len(ingred_list)), dtype=np.int8)
 
     label_df_np = []
     for i, item in enumerate(data):
@@ -32,10 +41,14 @@ def make_df(file_path):
     df_pd = pd.DataFrame(feature_df_np, columns=ingred_list)
     df_pd['cuisine'] = label_df_np
 
+    df_pd = df_pd.sample(frac=1, random_state=1)
+
     # Drop NAN
     # print '[INFO] Before dropna, Shape:', df_pd.shape
     # df_pd = drop_na(df_pd)
     # print '[INFO] Ater dropna, Shape:', df_pd.shape
+
+    # sorted_ingred = sorted(list(set(df_pd['cuisine'].tolist())))
 
     one_hot = pd.get_dummies(df_pd['cuisine'])
     one_hot = one_hot.values.tolist()
@@ -43,19 +56,20 @@ def make_df(file_path):
     df_pd = df_pd.drop('cuisine', axis=1)
     df_pd['labels'] = one_hot
 
-    print len(df_pd['labels'][0])
+    # print len(df_pd['labels'][0])
 
     # Calculate ones
     # summms = df_pd.sum(axis=1) + 6715
     # print summms.sum(axis=0)
 
     # df_pd.to_csv('feature_sheet.csv')
-    print 'Feature sheet generated'
+    # print 'Feature sheet generated'
 
     rows = df_pd.shape[0]
 
-    train_df = df_pd.iloc[:int(0.8*rows), :]
-    test_df = df_pd.iloc[int(0.8*rows):]
+    split = 0.95
+    train_df = df_pd.iloc[:int(split*rows) + 1, :]
+    test_df = df_pd.iloc[int(split*rows) + 1:]
 
     # print train_df.shape
     # print test_df.shape
@@ -64,8 +78,83 @@ def make_df(file_path):
     train_labels = list(np.array(train_df.iloc[:, -1]))
     test_features = list(np.array(test_df.iloc[:, :-1]))
     test_labels = list(np.array(test_df.iloc[:, -1]))
+    print 'Size of train set: {} \n Size of test set: {}'.format(train_df.shape[0], test_df.shape[0])
 
     return train_features, train_labels, test_features, test_labels
 
+def init_df():
+    with open('train.json') as data_file:
+        data = json.load(data_file)
+
+    ingred_list = set()
+    for item in data:
+        ingred_list |= set(item['ingredients'])
+
+    ingred_list = list(ingred_list)
+    print len(ingred_list)
+
+    ingred_dict = dict()
+    for i, ingredient in enumerate(ingred_list):
+        ingred_dict[ingredient] = i
+
+    feature_df_np = np.zeros((len(data), len(ingred_list)), dtype=np.int8)
+
+    df_pd = pd.DataFrame(feature_df_np, columns=ingred_list)
+    return ingred_list, ingred_dict
+
+
+def make_df(filename):
+    with open(filename) as data_file:
+        data = json.load(data_file)
+
+    ingred_list, ingred_dict = init_df()
+    feature_df_np = np.zeros((len(data), len(ingred_list)), dtype=np.int8)
+
+    df_pd = pd.DataFrame(feature_df_np, columns=ingred_list)
+
+    if filename == 'train.json':
+        label_df_np = []
+        for i, item in enumerate(data):
+            label_df_np.append(item['cuisine'])
+            for ingredient in item['ingredients']:
+                feature_df_np[i][ingred_dict[ingredient]] = 1
+        df_pd = df_pd.sample(frac=1, random_state=1)
+        df_pd['cuisine'] = label_df_np
+
+        # Drop NAN
+        # print '[INFO] Before dropna, Shape:', df_pd.shape
+        # df_pd = drop_na(df_pd)
+        # print '[INFO] Ater dropna, Shape:', df_pd.shape
+
+        # sorted_ingred = sorted(list(set(df_pd['cuisine'].tolist())))
+
+        one_hot = pd.get_dummies(df_pd['cuisine'])
+        one_hot = one_hot.values.tolist()
+
+        df_pd = df_pd.drop('cuisine', axis=1)
+        df_pd['labels'] = one_hot
+
+        features = list(np.array(df_pd.iloc[:, :-1]))
+        labels = list(np.array(df_pd.iloc[:, -1]))
+        return features, labels
+
+    elif filename == 'test.json':
+        id = []
+        for i, item in enumerate(data):
+            id.append(item['id'])
+            for ingredient in item['ingredients']:
+                try:
+                    feature_df_np[i][ingred_dict[ingredient]] = 1
+                except:
+                    pass
+
+        # df_pd['id'] = id
+        # print np.sum(df_pd, axis=0)
+        # print df_pd.head()
+        features = list(np.array(df_pd))
+        return features, id
+
+
 if __name__ == "__main__":
-    tr_f, tr_l, ts_f, ts_l = make_df('train.json')
+    # tra_X, y = make_df('train.json')
+    tst_X = make_df('test.json')
--- what's cooking/train_model.py
+++ what's cooking/train_model.py
@@ -1,109 +1,245 @@
 import tensorflow as tf
-import create_df
 import numpy as np
+import  time
+import datetime
+import math
+import matplotlib.pyplot as plt
+
+import create_df
+
 
 class Train_Model(object):
+    tf.set_random_seed(0)
 
+    def __init__(self, filename, train_test_split=True):
 
-    def __init__(self):
+        self.filename = filename
+        self.train_test_split = train_test_split
+        self.ids = []
+
         self.train_features = None
-        self.train_labels= None
-        self.test_features= None
-        self.test_labels= None
-        self.x = tf.placeholder(tf.float32)
-        self.y = tf.placeholder(tf.float32)
+        self.train_labels = None
+        self.test_features = None
+        self.test_labels = None
+
+        self.hidden_layer1 = dict()
+        self.hidden_layer2 = dict()
+        self.hidden_layer3 = dict()
+        self.hidden_layer4 = dict()
+        self.output_layer = dict()
+
+        self.x = tf.placeholder(tf.float32, name='x')
+        self.y = tf.placeholder(tf.float32, name='y')
+        self.lr = tf.placeholder(tf.float32, name='lr')
+        self.pkeep = tf.placeholder(tf.float32, name='pkeep')
 
     def get_data(self):
-        return create_df.make_df('train.json')
+        if self.train_test_split:
+            result = create_df.make_train_test_df(self.filename)
+            self.train_features, self.train_labels, self.test_features, self.test_labels = result
+        else:
+            if self.filename == 'test.json':
+                self.train_features, self.ids = create_df.make_df(self.filename)
+            else:
+                self.train_features, self.train_labels = create_df.make_df(self.filename)
+
 
     def layout(self):
-        self.train_features, self.train_labels, self.test_features, self.test_labels = self.get_data()
+        # self.train_features, self.train_labels, self.test_features, self.test_labels = self.get_data()
+        self.get_data()
 
-        print self.train_features[0].shape
-        hidden_layer1 = dict()
-        hidden_layer1['nodes'] = 6714
-        hidden_layer1['weights'] = tf.Variable(tf.truncated_normal([len(self.train_features[0]),hidden_layer1['nodes']],stddev=0.1))
-        hidden_layer1['bias'] = tf.Variable(tf.zeros([hidden_layer1['nodes']]))
+        self.hidden_layer1['nodes'] = 100
+        self.hidden_layer1['weights'] = tf.Variable(
+            tf.truncated_normal([len(self.train_features[0]), self.hidden_layer1['nodes']], stddev=0.1))
+        self.hidden_layer1['bias'] = tf.Variable(tf.zeros([self.hidden_layer1['nodes']]))
+        prev_nodes = self.hidden_layer1['nodes']
 
-        hidden_layer2 = dict()
-        hidden_layer2['nodes'] = 3357
-        hidden_layer2['weights'] = tf.Variable(tf.truncated_normal([hidden_layer1['nodes'],hidden_layer2['nodes']],stddev=0.1))
-        hidden_layer2['bias'] = tf.Variable(tf.zeros([hidden_layer2['nodes']]))
+        self.hidden_layer2['nodes'] = 60
+        self.hidden_layer2['weights'] = tf.Variable(
+            tf.truncated_normal([prev_nodes, self.hidden_layer2['nodes']], stddev=0.1))
+        self.hidden_layer2['bias'] = tf.Variable(tf.zeros([self.hidden_layer2['nodes']]))
+        prev_nodes = self.hidden_layer2['nodes']
 
-        hidden_layer3 = dict()
-        hidden_layer3['nodes'] = 1600
-        hidden_layer3['weights'] = tf.Variable(tf.truncated_normal([hidden_layer2['nodes'],hidden_layer3['nodes']],stddev=0.1))
-        hidden_layer3['bias'] = tf.Variable(tf.zeros([hidden_layer3['nodes']]))
+        self.hidden_layer3['nodes'] = 50
+        self.hidden_layer3['weights'] = tf.Variable(
+            tf.truncated_normal([prev_nodes, self.hidden_layer3['nodes']], stddev=0.1))
+        self.hidden_layer3['bias'] = tf.Variable(tf.zeros([self.hidden_layer3['nodes']]))
+        prev_nodes = self.hidden_layer3['nodes']
 
-        output_layer = dict()
-        output_layer['nodes'] = 20
-        output_layer['weights'] = tf.Variable(tf.truncated_normal([hidden_layer3['nodes'],output_layer['nodes']],stddev=0.1))
-        output_layer['bias'] = tf.Variable(tf.zeros([output_layer['nodes']]))
+        self.hidden_layer4['nodes'] = 40
+        self.hidden_layer4['weights'] = tf.Variable(
+            tf.truncated_normal([prev_nodes, self.hidden_layer4['nodes']], stddev=0.1))
+        self.hidden_layer4['bias'] = tf.Variable(tf.zeros([self.hidden_layer4['nodes']]))
+        prev_nodes = self.hidden_layer4['nodes']
 
-        return hidden_layer1,hidden_layer2,hidden_layer3,output_layer
+        self.output_layer['nodes'] = 20
+        self.output_layer['weights'] = tf.Variable(
+            tf.truncated_normal([prev_nodes, self.output_layer['nodes']], stddev=0.1))
+        self.output_layer['bias'] = tf.Variable(tf.zeros([self.output_layer['nodes']]))
 
     def graph_gen(self, data):
 
-        hidden_layer1, hidden_layer2 , hidden_layer3, output_layer = self.layout()
-        layer1 = tf.add(tf.matmul(data,hidden_layer1['weights']), hidden_layer1['bias'])
+        self.layout()
+
+        layer1 = tf.add(tf.matmul(data, self.hidden_layer1['weights']), self.hidden_layer1['bias'])
+        # layer1 = tf.nn.tanh(layer1)
         layer1 = tf.nn.relu(layer1)
+        layer1 = tf.nn.dropout(layer1, self.pkeep)
+        prev_layer = layer1
 
-        layer2 = tf.add(tf.matmul(layer1, hidden_layer2['weights']), hidden_layer2['bias'])
+        layer2 = tf.add(tf.matmul(prev_layer, self.hidden_layer2['weights']), self.hidden_layer2['bias'])
+        # layer2 = tf.nn.tanh(layer2)
         layer2 = tf.nn.relu(layer2)
+        layer2 = tf.nn.dropout(layer2, self.pkeep)
+        prev_layer = layer2
 
-        layer3 = tf.add(tf.matmul(layer2, hidden_layer3['weights']), hidden_layer3['bias'])
+        layer3 = tf.add(tf.matmul(prev_layer, self.hidden_layer3['weights']), self.hidden_layer3['bias'])
+        # layer3 = tf.nn.tanh(layer3)
         layer3 = tf.nn.relu(layer3)
+        layer3 = tf.nn.dropout(layer3, self.pkeep)
+        prev_layer = layer3
 
-        output = tf.add(tf.matmul(layer3, output_layer['weights']), output_layer['bias'])
+        layer4 = tf.add(tf.matmul(prev_layer, self.hidden_layer4['weights']), self.hidden_layer4['bias'])
+        # layer4 = tf.nn.tanh(layer3)
+        layer4 = tf.nn.relu(layer4)
+        layer4 = tf.nn.dropout(layer4, self.pkeep)
+        prev_layer = layer4
+
+        output = tf.add(tf.matmul(prev_layer, self.output_layer['weights']), self.output_layer['bias'])
 
         return output
 
     def train_nn(self):
 
-        learning_rate = 0.01
+        # learning_rate = 0.0001
+        beta = 0.01
         prediction = self.graph_gen(self.x)
         softmax_prediction = tf.nn.softmax(prediction)
 
         cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=self.y))
-        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(cost)
 
-        correct = tf.equal(tf.argmax(softmax_prediction,1),tf.argmax(self.y,1))
-        accuracy = tf.reduce_mean(tf.cast(correct,tf.float32))
+        correct = tf.equal(tf.argmax(softmax_prediction, 1), tf.argmax(self.y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
-        n_epochs = 100
+        n_epochs = 200
         batch_size = 100
+
+        train_acc_to_plot = []
+        test_acc_to_plot = []
+        cost_to_plot = []
+
+        fig = plt.figure()
+
+        saver = tf.train.Saver()
+
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
+            # plt.axis([0, n_epochs, 0, 1])
+            # plt.ion()
+            max_test_acc = 0.0
+
             for epoch in range(n_epochs):
                 epoch_loss = 0
                 i = 0
+
+                # learning rate decay
+                max_learning_rate = 0.01
+                min_learning_rate = 0.0001
+                decay_speed = 2000.0
+                learning_rate = min_learning_rate + (max_learning_rate - min_learning_rate) \
+                                                    * math.exp(-epoch / decay_speed)
                 while i < len(self.train_features):
                     start = i
                     end = i + batch_size
                     batch_x = np.array(self.train_features[start:end])
                     batch_y = np.array(self.train_labels[start:end])
 
-                    temp, c = sess.run([optimizer, cost], feed_dict={self.x: batch_x, self.y: batch_y})
+                    temp, c = sess.run([optimizer, cost], feed_dict={self.x: batch_x, self.y: batch_y,
+                                                                     self.lr: learning_rate, self.pkeep: 0.85})
 
                     epoch_loss += c
-
                     i += batch_size
 
-                train_accuracy = accuracy.eval({self.x:self.train_features, self.y:self.train_labels})
+                cost_to_plot.append(epoch_loss)
 
-                print 'epoch:',epoch,'\taccuracy:',train_accuracy, '\tloss:', epoch_loss
+                # if epoch % 5 == 0:
+                train_accuracy = accuracy.eval(
+                    {self.x: self.train_features, self.y: self.train_labels, self.pkeep: 1.0})
+                train_acc_to_plot.append(train_accuracy)
+
+                if self.train_test_split:
+                    test_accuracy = accuracy.eval({self.x: self.test_features, self.y: self.test_labels,
+                                                   self.pkeep: 1.0})
+                    if test_accuracy > max_test_acc:
+                        max_test_acc = test_accuracy
+                    test_acc_to_plot.append(test_accuracy)
+
+                    print 'epoch:', epoch, '\ttrain_accuracy:', train_accuracy, '\ttest_accuracy:', test_accuracy, \
+                                        '\tloss:', epoch_loss, '\tmaximum test accuracy:', max_test_acc
+                else:
+                    print 'epoch:', epoch, '\ttrain_accuracy:', train_accuracy, \
+                        '\tloss:', epoch_loss, '\tlearning_rate:', learning_rate
 
 
-train_model_obj = Train_Model()
+                    # plt.scatter(epoch + 1, epoch_loss, c='r')
+            #     plt.scatter(epoch + 1, test_accuracy, c='y')
+            #     plt.scatter(epoch + 1, train_accuracy, c='b')
+            #     plt.pause(0.001)
+            #
+            # plt.ioff()
+            # plt.show()
+                if test_accuracy > 0.78:
+                    break
+
+            save_path = saver.save(sess, "./saved_models/model_v1.ckpt")
+            print("Model saved in file: %s" % save_path)
+
+        ax1 = fig.add_subplot(221)
+        ax1.plot(range(n_epochs), train_acc_to_plot, label='train', c='b')
+        ax1.set_ylim([0.97, 1.0])
+        ax1.legend()
+
+        if self.train_test_split:
+            ax2 = fig.add_subplot(222)
+            ax2.plot(range(n_epochs), test_acc_to_plot, label='test', c='y')
+            ax2.set_ylim([0.75, 0.79])
+            ax2.legend()
+
+        ax3 = fig.add_subplot(223)
+        ax3.plot(range(n_epochs), cost_to_plot, label='cost', c='r')
+        ax3.set_ylim([0, 100])
+        ax3.legend()
+
+        plt.show()
+        # print "maximum test accuracy:", max_test_acc
+
+    def predict_nn(self, actual_data=None):
+
+        prediction = self.graph_gen(self.x)
+        print 'len:', len(self.train_features)
+
+        saver = tf.train.Saver()
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            saver.restore(sess, "./saved_models/model_v1.ckpt")
+            output_file = './outputs/output' + str(datetime.datetime.now()) + '.csv'
+            result = sess.run(tf.argmax(prediction.eval(feed_dict={self.x: self.train_features,
+                                                                   self.pkeep: 1}), 1))
+            with open(output_file, 'w') as f:
+                f.write('id,cuisine\n')
+                for i, item in enumerate(result):
+                    if i % 1000 == 0:
+                        print i, 'id:', self.ids[i], 'result:', result, create_df.one_hot_to_real(item)
+                    f.write(str(self.ids[i]) + ',' + create_df.one_hot_to_real(item) + '\n')
+
+
+train_model_obj = Train_Model('train.json')
 train_model_obj.train_nn()
 
+# train_model_obj = Train_Model('train.json', train_test_split=False)
+# train_model_obj.train_nn()
 
-
-
-
-
-
-
-        
-
-
+# predict_model_obj = Train_Model('test.json', train_test_split=False)
+# predict_model_obj.predict_nn()
